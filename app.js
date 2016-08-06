9f80feddd0028a917e28786eb2c023fe22e57fe2
Taras Novak <taras.novak@randomfractals.com>
2016-08-06 11:20:02 -0500

    added env vars loading setup for twitter keys

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// initialize env vars
+require('dotenv').config();
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
