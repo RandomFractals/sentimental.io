9d91d73d70491bc9ca8330e989689b30074e5134
Taras Novak <taras.novak@randomfractals.com>
2016-08-06 11:35:12 -0500

    added twitter module and config

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var Twitter = require('twitter');
+
+var twitterClient = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
