086b40e59a93a24a4c0a200c57ba8f38adfd8e44
Taras Novak <taras.novak@randomfractals.com>
2016-08-06 17:07:15 -0500

    got Big Blue Watson Alchemy Lang API  config and test call going

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var Twitter = require('twitter');
+var Watson = require('watson-developer-cloud');
 
 var twitterClient = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -11,19 +12,35 @@ var twitterClient = new Twitter({
 });
 
 
+var alchemyLangClient = Watson.alchemy_language({
+  api_key: process.env.ALCHEMY_API_KEY
+});
+
 router.get('/app/tweets', function(req, res, next) {
+
+  // get tweets
   twitterClient.get('search/tweets', 
     { 
       q: "sentiments",
       count: "2"
     }, 
-    
+
     function(error, tweets, response){
     if (error) throw error;
     //console.log(tweets);
     //console.log(response);
     res.send(tweets.statuses);
   });
+
+  // test alchemy hookup
+  alchemyLangClient.sentiment({text: 'Word on the street Watson is good at text analytics'}, 
+    function (err, response) {
+      if (err)
+        console.log('error:', err);
+      else
+        console.log(JSON.stringify(response, null, 2));
+    });
+
 });
 
 module.exports = router;
