b3f174b03dab1386f71a5317757d84670e8e7ab5
Taras Novak <taras.novak@randomfractals.com>
2016-08-21 11:30:27 -0500

    moved app content html setup to app.html

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,4 +1,6 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app';
+import { enableProdMode } from '@angular/core';
 
+//enableProdMode(); // uncomment for production
 bootstrap(AppComponent);
