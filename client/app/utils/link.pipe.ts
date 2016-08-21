ec5529067e0f85a6c6f3afee4bd3b8c31a2b5b0c
Taras Novak <taras.novak@randomfractals.com>
2016-08-21 10:10:33 -0500

    added user mentions link rewrite to link.pipe

--- client/app/utils/link.pipe.ts
+++ client/app/utils/link.pipe.ts
@@ -1,16 +1,24 @@
 import { Pipe, PipeTransform  } from '@angular/core';
 
 /**
- * Defines simple url rewrite pipe 
- * that converts http* strings into html links.
+ * Simple url and twitter user links rewrite pipe.
  */
 @Pipe({name: 'link'})
 export class LinkPipe implements PipeTransform {
 
   transform (text: string): string {
+    // rewrite urls
     let urlRegEx = /(https?:\/\/[^\s]+)/g;
-    return text.replace( urlRegEx, function(url) {
+    text = text.replace( urlRegEx, function(url) {
       return `<a href="${url}" target="_blank">${url}</a>`;
-    })
+    });
+
+    // rewrite user mentions
+    let userRegEx = /(\@[^\s]+)/g;
+    text = text.replace( userRegEx, function(user) {
+      return `<a href="https://www.twitter.com/${user.substring(1).replace(':', '')}" target="_blank">${user}</a>`;
+    });
+
+   return text; 
   }
 }
