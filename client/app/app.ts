b93d80427029612fa4e5af48f69ef77da72ad0e3
Taras Novak <taras.novak@randomfractals.com>
2016-08-22 01:25:28 -0500

    added window scroll handler to wire pagination in later

--- client/app/app.ts
+++ client/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 
 import { TwitterService } from './shared/twitter.service'
 
@@ -21,5 +21,14 @@ export class AppComponent {
   search():void {
     console.log(`search: ${this.term}`);
     this._twitterService.search(this.term);
+  }
+
+  @HostListener('window:onscroll', ['$event'])
+  onWindowScroll(event) {
+    // TODO: check scrollbar position
+    // and get the next set of tweets if at the bottom of the page
+    let doc = event.target.documentElement;
+    console.log(event);
+    console.log(`scrollTop: ${doc.scrollTop} scrollHeight: ${doc.scrollHeight} clientTop: ${doc.clientTop} clientHeight: ${doc.clientHeight} offsetHeight: ${doc.offsetHeight}`);
   }
 }
