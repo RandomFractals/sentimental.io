8703a0d5495a8e5236f0f9b527bccf82e57bd554
Taras Novak <taras.novak@randomfractals.com>
2016-08-21 20:22:55 -0500

    wired alpha search form

--- client/app/app.ts
+++ client/app/app.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { TwitterService } from './shared/twitter.service'
 
 @Component({
   selector: 'twitter-app',
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Twitter Sentiments';
+  // app title
+  title:string = 'Twitter Sentiments';
+
+  // global search term
+  @Input() term:string = '';
+
+  constructor(private _twitterService: TwitterService) { 
+  }
+  
+  search():void {
+    console.log(`search: ${this.term}`);
+    this._twitterService.getTweets(this.term);    
+  }
 }
